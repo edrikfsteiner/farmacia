ad1a2637f5a6aa3d918437d86be4538519a12068
edrikfsteiner <163454021+edrikfsteiner@users.noreply.github.com>
2025-01-03 15:01:59 -0300

    Delete farmacia directory

--- farmacia/src/main/java/com/farmacia/repository/MedicamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.farmacia.repository;
-
-import com.farmacia.model.Medicamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicamentoRepository extends JpaRepository<Medicamento, Long> {}
--- farmacia/src/main/java/com/farmacia/repository/ReceitaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.farmacia.repository;
-
-import com.farmacia.model.Receita;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReceitaRepository extends JpaRepository<Receita, Long> {}
